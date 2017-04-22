ab0cef7a6896aec3666ad91dc7554beb3e7ffce1
Silence <374619540@qq.com>
2017-04-22 11:10:12 +0800

    HelloWorld

---
+++ Android/HelloWorld/app/src/main/java/com/silence/helloworld/HelloWorldActivity.java
@@ -0,0 +1,18 @@
+package com.silence.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class HelloWorldActivity extends AppCompatActivity {
+
+    private static final String TAG = "HelloWorldActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_world);
+        Log.i(TAG,"V") ;
+
+    }
+}
